c0e8a2f53348ea30afe5f332875bd09f7815c84a
DANG PHAM <dangph2011@gmail.com>
2018-09-13 22:44:42 +0700

    remove duplicate css include

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -113,8 +113,6 @@ desired effect
     <!-- AdminLTE App -->
     <script src="bower_components/admin-lte/dist/js/adminlte.min.js"></script>
 
-    <script src="bower_components/admin-lte/dist/js/adminlte.min.js"></script>
-
     <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
      user experience. -->
