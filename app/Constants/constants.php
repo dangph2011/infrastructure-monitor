ce051dd5ff3339c96dbe9fd1d29fa5aa3b8405d2
DANG PHAM <dangph2011@gmail.com>
2018-09-19 15:23:22 +0700

    Add constant file

---
+++ app/Constants/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+// define value type of item (items table)
+
+define("ITEM_VALUE_TYPE_FLOAT", 0);
+define("ITEM_VALUE_TYPE_CHARACTER", 1);
+define("ITEM_VALUE_TYPE_LOG", 2);
+define("ITEM_VALUE_TYPE_UNSIGNED", 3);
+define("ITEM_VALUE_TYPE_TEXT", 4);
+
+//define graph type (graph table)
+define("GRAPH_TYPE_NORMAL", 0);
+define("GRAPH_TYPE_STACKED", 1);
+define("GRAPH_TYPE_PIE", 2);
+define("GRAPH_TYPE_EXPLODED", 3);
+
+define('GRAPH_ITEM_SIMPLE',			0);
+define('GRAPH_ITEM_SUM',			2);
+
