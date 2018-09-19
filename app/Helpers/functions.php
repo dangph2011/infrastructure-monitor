85463e381e522bd403ad02fd0bbaf9d5882e6aff
DANG PHAM <dangph2011@gmail.com>
2018-09-19 18:25:12 +0700

    Add Helpers function

---
+++ app/Helpers/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+function getSelectorOption($options) {
+        $selector = collect();
+        foreach ($options as $key => $value) {
+            if ($value == "1m") {
+                $selector->push($this->createSelectorOption('month', 'backward', 1, '1m'));
+            } elseif ($value == "3m") {
+                $selector->push($this->createSelectorOption('month', 'backward', 3, '3m'));
+            } elseif ($value == "6m") {
+                $selector->push($this->createSelectorOption('month', 'backward', 6, '6m'));
+            } elseif ($value == "ytd") {
+                $selector->push($this->createSelectorOption('year', 'todate', 1, 'YTD'));
+            } elseif ($value == "1y") {
+                $selector->push($this->createSelectorOption('year', 'backward', 1, '1y'));
+            }
+        }
+        $selector->push(['step'=>'all']);
+        return $selector;
+    }
+
+function createSelectorOption($step, $stepMode, $count, $lable)
+    {
+        return collect([
+            "step" => $step,
+            "stepmode" => $stepMode,
+            "count" => $count,
+            "lable" => $lable,
+        ]);
+    }
