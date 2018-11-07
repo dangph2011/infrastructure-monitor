829d6b2800f019944e8396f6e2b7c3b1c238de7b
DANG PHAM <dangph2011@gmail.com>
2018-11-07 22:55:09 +0700

    Fix path bower component

--- resources/views/layouts/sidebar_menu.blade.php
+++ resources/views/layouts/sidebar_menu.blade.php
@@ -1,10 +1,52 @@
 <ul class="sidebar-menu" data-widget="tree">
     <li class="header">MAIN NAVIGATION</li>
     <!-- Optionally, you can add icons to the links -->
-    <li class="active"><a href="/graph"><i class="fa fa-pie-chart"></i> <span>Graph</span></a></li>
-    <li class="active"><a href="/problem"><i class="fa fa-exclamation-circle"></i> <span>Problem</span></a></li>
-    <li class="active"><a href="/trigger"><i class="fa fa-stethoscope"></i> <span>Trigger</span></a></li>
-    {{-- <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+    {{--
+    <li class="active"><a href="/graph"><i class="fa fa-pie-chart"></i> <span>Graph</span></a></li> --}} {{--
+    <li class="active"><a href="/problem"><i class="fa fa-exclamation-circle"></i> <span>Problem</span></a></li> --}} {{--
+    <li class="active"><a href="/trigger"><i class="fa fa-stethoscope"></i> <span>Trigger</span></a></li> --}}
+    <li class="active"><a href="#"><i class="fa fa-link"></i><span>Topo</span></a></li>
+
+    <li class="treeview">
+        <a href="#"><i class="fa fa-pie-chart"></i> <span>Graphs</span>
+                <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+                </span>
+                </a>
+        <ul class="treeview-menu">
+            <li><a href="#">Create graph</a></li>
+            <li><a href="/graph">View graph</a></li>
+        </ul>
+    </li>
+
+    <li class="treeview">
+        <a href="#"><i class="fa fa-exclamation-circle"></i> <span>Problems</span>
+                <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+                </span>
+                </a>
+        <ul class="treeview-menu">
+            <li><a href="#">Create trigger</a></li>
+            <li><a href="/problems/problem">Problem</a></li>
+            <li><a href="/problems/trigger">Trigger</a></li>
+        </ul>
+    </li>
+
+    <li class="treeview">
+        <a href="#"><i class="fa fa-sticky-note-o"></i> <span>Reports</span>
+                    <span class="pull-right-container">
+                    <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                    </a>
+        <ul class="treeview-menu">
+            <li><a href="#">Create report</a></li>
+            <li><a href="#">View report</a></li>
+        </ul>
+    </li>
+
+
+    {{--
+    <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li> --}} {{--
     <li class="treeview">
         <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
             <span class="pull-right-container">
