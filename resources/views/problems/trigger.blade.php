2cfe3332003ff995f6ccfe66ab721a61c5b3f045
DANG PHAM <dangph2011@gmail.com>
2018-11-09 23:35:16 +0700

    fix get route

--- resources/views/problems/trigger.blade.php
+++ resources/views/problems/trigger.blade.php
@@ -10,7 +10,7 @@
 
 <div class="container-fluid">
     <div class="row">
-        <form method="GET" action="problems/trigger">
+        <form method="GET" action="/problems/trigger">
             {{csrf_field()}}
             <div class='col-md-4'>
                 <div class="form-group">
