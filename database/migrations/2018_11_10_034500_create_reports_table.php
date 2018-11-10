1e075567e9a0eeb1ada4d7aee3c3e96ebe844f94
DANG PHAM <dangph2011@gmail.com>
2018-11-10 11:05:25 +0700

    Add reports and reports_graphs table

---
+++ database/migrations/2018_11_10_034500_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('reportid');
+            $table->string('name', 255)->default('');
+            $table->string('schedule', 128)->default('* * * * *');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
